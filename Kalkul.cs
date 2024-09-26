0ad9b9e08d12d02740c8cae764c3dd4b8672a6f8
dmitrij6523 <158767012+dmitrij6523@users.noreply.github.com>
2024-09-26 09:12:49 +0300

    Add files via upload

---
+++ Kalkul.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulyator
+{
+    internal class Kalkul
+    {
+
+        double number_one;
+        double number_two;
+
+        public void plus(double number_one, double number_two)
+        {
+            Console.WriteLine(number_one + number_two);
+        }
+        public void minus(double number_one, double number_two)
+        {
+            Console.WriteLine(number_one - number_two);
+        }
+        public void multiplication(double number_one, double number_two)
+        {
+            Console.WriteLine(number_one * number_two);
+        }
+        public void division(double number_one, double number_two)
+        {
+            if (number_two == 0)
+            {
+                Console.WriteLine("На 0 делить нельзя");
+            }
+            else
+            {
+                Console.WriteLine(number_one / number_two);
+            }
+
+        }
+
+        public void rounding(double number_one, double number_two) //Округление
+        {
+            Console.WriteLine(Math.Round(number_one));
+            Console.WriteLine(Math.Round(number_two));
+        }
+
+        public void arksin(double number_one, double number_two) //Арксинус
+        {
+            Console.WriteLine(Math.Atan(number_one));
+        }
+
+        public void degree(double number_one, double number_two) //Степень
+        {
+            Console.WriteLine(Math.Pow(number_one, number_two));
+        }
+
+        public void square(double number_one, double number_two) //Квадрат
+        {
+            Console.WriteLine(Math.Sqrt(number_one));
+            Console.WriteLine(Math.Sqrt(number_two));
+        }
+        public void sin(double number_one, double number_two) //Синус
+        {
+            Console.WriteLine(Math.Sin(number_one));
+            Console.WriteLine(Math.Sin(number_two));
+        }
+        public void tan(double number_one, double number_two) //Тангенс
+        {
+            Console.WriteLine(Math.Tan(number_one));
+        }
+        public void squreArea(double number_one, double number_two) //Площадь
+        {
+            Console.WriteLine(Math.Pow(number_one, 2));
+            Console.WriteLine(Math.Pow(number_two, 2));
+        }
+
+        public void squarePerimeter(double number_one, double number_two) //Периметр
+        {
+            Console.WriteLine(4 * number_one);
+            Console.WriteLine(4 * number_two);
+        }
+
+        public void circleDiameter(double num_one, double num_two) //Диаметр
+        {
+            Console.WriteLine(2 * number_one);
+            Console.WriteLine(2 * number_two);
+        }
+
+        public void circleRadius(double number_one, double number_two) //Радиус
+        {
+            Console.WriteLine(number_one / 2);
+            Console.WriteLine(number_two / 2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using Kalkulyator;
+
+first:
+
+Kalkul clas = new Kalkul();
+
+string solo;
+
+Console.WriteLine("Введите первое число");
+double x = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Введите действие");
+solo = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+double y = Convert.ToDouble(Console.ReadLine());
+
+
+
+switch (solo)
+{
+    case "+":
+        clas.plus(x, y);
+        goto first;
+    case "-":
+        clas.minus(x, y);
+        goto first;
+    case "*":
+        clas.multiplication(x, y);
+        goto first;
+    case "/":
+        clas.division(x, y);
+        goto first;
+
+    case "Округление":
+        clas.rounding(x, y);
+        goto first;
+    case "Арксинус":
+        clas.arksin(x, y);
+        goto first;
+    case "Степень":
+        clas.degree(x, y);
+        goto first;
+    case "Квадрат":
+        clas.square(x, y);
+        goto first;
+    case "Синус":
+        clas.sin(x, y);
+        goto first;
+    case "Тангенс":
+        clas.tan(x, y);
+        goto first;
+    case "Площадь":
+        clas.squreArea(x, y);
+        goto first;
+    case "Периметр":
+        clas.squarePerimeter(x, y);
+        goto first;
+    case "Диаметр":
+        clas.circleDiameter(x, y);
+        goto first;
+    case "Радиус":
+        clas.circleRadius(x, y);
+        goto first;
+}
